a7b9bfc68faad7c527f257b11d61d6ef114a3a96
Cezary Grzanka <cezarygrzanka@gmail.com>
2022-03-23 20:52:27 +0100

    Remove unused for now async utils

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -3,7 +3,3 @@ import { Logger } from './logger.types';
 declare module '@cgsh/utils/logger' {
   const logger: Logger;
 }
-
-declare module '@cgsh/utils/async-utils' {
-  export const logger: Logger;
-}
